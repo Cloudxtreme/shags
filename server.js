32754c2de0577ee6b61286270e0e8fcdd6010dcd
Nektro <nektromc@gmail.com>
2017-12-18 00:32:12 -0500

    rework express to use assets folder and update protocol

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ const web_path = `${__dirname}/www`;
 
 //
 app.get('/', function(req, res){ res.sendFile(web_path + '/index.html'); });
-app.get('/app.min.css', function(req, res){ res.sendFile(web_path + '/app.min.css'); });
-app.get('/app.js', function(req, res){ res.sendFile(web_path + '/app.js'); });
+app.use('/assets', express.static('www/assets', { dotfiles: 'allow' }))
 
 //
 function sendMessage(app, event, data) {
-    io.emit('shags_message', { app, event, data });
+    io.to(app).emit('shags_message', { app, event, data });
 }
 
 //
@@ -32,18 +31,21 @@ io.on('connection', function(socket) {
     socket.on('disconnect', function() {
         const pl = players.get(socket);
         players.delete(socket);
-        if (pl.nickname === "") {
-            sendMessage('user', 'leave', pl.nickname);
-            console.log(`<- There are now ${players.size} players`);
-        }
+        sendMessage(pl.game, 'user_leave', pl.nickname);
+        console.log(`<- There are now ${players.size} players`);
     });
     socket.on('shags_message', function(message) {
         switch (message.app) {
             case 'user': return (() => {
                 switch (message.event) {
-                    case 'nickname': return (() => {
+                    case 'set_nickname': return (() => {
                         players.get(socket).nickname = message.data;
-                        sendMessage('user', 'join', message.data);
+                    })();
+                    case 'goto_room': return (() => {
+                        socket.join(message.data);
+                        const pl = players.get(socket);
+                        pl.game = message.data;
+                        sendMessage(message.data, 'user_join', pl.nickname);
                     })();
                 }
             })();
